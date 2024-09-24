55eed1208b26930e457ed894e663d80590d25fcb
Daniel Dager <danieldager@Daniels-MacBook-Pro.local>
2024-09-24 16:45:24 +0200

    generate morpheme and grapheme datasets

--- data/initial_dataset/extract.py
+++ data/initial_dataset/extract.py
@@ -1,12 +1,12 @@
-import os, sys
+import os
 import pandas as pd
 
 # get list of all files in the directory
-files = os.listdir('data/initial_datasets')
+files = os.listdir('data/initial_dataset')
 
 # load the data from csv files
 data = {}
 for file in files:
     if file.endswith('.csv'):
-        data[file.split('.')[0]] = pd.read_csv('data/initial_datasets/' + file)
+        data[file.split('.')[0]] = pd.read_csv('data/initial_dataset/' + file)
 
