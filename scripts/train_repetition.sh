912010b6dfa019a54741fa32c52f3a146620b045
Daniel Dager <danieldager@Daniels-MacBook-Pro-3.local>
2024-10-29 10:10:11 +0100

    update slurm script

--- scripts/train_repetition.sh
+++ scripts/train_repetition.sh
@@ -9,6 +9,23 @@
 #SBATCH --output=%x-%j.log            # Standard output and error log
 
 echo "Running job on $(hostname)"
+
+# create execution environment
+module purge                        
+module load miniconda3/24.3.0-ui7c
+eval "$(conda shell.bash hook)"
+
+# create environment only if it doesn't exist
+if ! conda env list | grep -q "^swpm "; then
+    conda create -n swpm python=3.12 -y
+fi
+
+# activate environment and install dependencies
+conda activate swpm
+pip install -r requirements.txt --quiet
+python -m spacy download en_core_web_lg --no-deps
+
+# print environment information
 echo "python: $(which python)"
 echo "python-version $(python -V)"
 echo "CUDA_DEVICE: $CUDA_VISIBLE_DEVICES"
@@ -17,14 +34,6 @@ echo "CUDA_DEVICE: $CUDA_VISIBLE_DEVICES"
 python -c "import torch; print(f'CUDA available: {torch.cuda.is_available()}')"
 python -c "import torch; print(f'cuda device: {torch.cuda.current_device()}')"
 export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
-
-# create execution environment
-module purge                        
-module load miniconda3/24.3.0-ui7c
-conda create -n swpm python=3.12
-conda activate swpm
-pip install -r requirements.txt
-python -m spacy download en_core_web_lg
 
 # launch your computation
 echo "computation start $(date)"
