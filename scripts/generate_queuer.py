5056e40ea0f1e3a81459973c8a9eb1a5268d31fb
RobinSobczyk <robin.sobczyk@laposte.net>
2025-02-05 16:02:36 +0100

    Updated grid search for JZ

--- scripts/generate_queuer.py
+++ scripts/generate_queuer.py
@@ -12,22 +12,28 @@ from swp.utils.task_queuer import create_jean_zay_train_repetition_queuer
 
 def get_grid():
     phoneme_to_id = get_phoneme_to_id()
-    grid: Grid = {
-        "model_class": ["Ua"],
-        "vocab_size": [len(phoneme_to_id)],
-        "start_token_id": [phoneme_to_id["<SOS>"]],
-        "cnn_args": [None],
-        "fold_id": list(range(5)),
-        "num_epochs": [50],
-        "batch_size": [1024],
-        "recur_type": ["RNN", "LSTM"],
-        "hidden_size": [2**4, 2**5, 2**5, 2**7, 2**8, 2**9],
-        "num_layers": [1],
-        "learning_rate": [1e-1, 1e-2, 1e-3, 1e-4],
-        "droprate": [0.0, 0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6],
-        "tf_ratio": [0.0, 0.05, 0.1, 0.2, 0.4, 0.5, 0.75, 1.0],
-        "include_stress": [False],
-    }
+    grid = Grid(
+        {
+            "model_class": ["Ua"],
+            "vocab_size": [len(phoneme_to_id)],
+            "start_token_id": [phoneme_to_id["<SOS>"]],
+            "cnn_args": [None],
+            "fold_id": list(range(5)),
+            "num_epochs": [70],
+            "batch_size": [4096],
+            "recur_type": ["LSTM"],  # ["RNN", "LSTM"],
+            "hidden_size": [128, 256, 512],  # [2**i for i in range(4, 10)],
+            "num_layers": [1],
+            "learning_rate": [10**-i for i in range(2, 5)],
+            "droprate": [0.5, 0.6, 0.7, 0.8],
+            "tf_ratio": [
+                0.0,
+                0.05,
+                0.1,
+            ],  # [0.0, 0.05, 0.1, 0.2, 0.5, 0.75, 1.0],
+            "include_stress": [False],
+        }
+    )
     return grid
 
 
