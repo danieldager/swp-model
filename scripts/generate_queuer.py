c0af01d45f0b411bedc5bc0355de2f7d2321ea39
RobinSobczyk <robin.sobczyk@laposte.net>
2025-02-09 16:51:26 +0100

    Fixed grid parameters not up to date

--- scripts/generate_queuer.py
+++ scripts/generate_queuer.py
@@ -25,7 +25,7 @@ def get_grid():
             "hidden_size": [64, 128],  # [256, 512],  # [2**i for i in range(4, 10)],
             "num_layers": [1],
             "learning_rate": [0.005, 0.001, 0.0005],  # [10**-i for i in range(2, 5)],
-            "droprate": [0.0, 0.05, 0.1],  # [0.5, 0.6, 0.7, 0.8],
+            "droprate": [0.0, 0.1, 0.2],  # [0.5, 0.6, 0.7, 0.8],
             "tf_ratio": [0.0],  # [
             #     0.0,
             #     0.05,
